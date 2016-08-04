7e7f673491c7a7abc5ff4c7f99ff416df7f2062b
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-04 02:19:18 -0400

    update deps, lint

--- index.js
+++ index.js
@@ -9,29 +9,28 @@
 
 var through = require('through2');
 var extend = require('extend-shallow');
-var prettify = require('js-beautify').html;
+var prettify = require('js-beautify');
+var defaults = {
+  indent_handlebars: true,
+  indent_inner_html: true,
+  preserve_newlines: false,
+  max_preserve_newlines: 1,
+  brace_style: 'expand',
+  indent_char: ' ',
+  indent_size: 2
+};
 
 module.exports = function(options) {
-  var opts = extend({}, options);
-
+  var opts = extend({}, defaults, options);
   return through.obj(function(file, enc, next) {
     if (file.isNull()) {
-      next();
+      next(null, file);
       return;
     }
 
     try {
       var str = file.contents.toString();
-      file.contents = new Buffer(prettify(str, extend({
-        indent_handlebars: true,
-        indent_inner_html: true,
-        preserve_newlines: false,
-        max_preserve_newlines: 1,
-        brace_style: 'expand',
-        indent_char: ' ',
-        indent_size: 2
-      }, opts)));
-
+      file.contents = new Buffer(prettify.html(str, opts));
     } catch (err) {
       next(err);
       return;
